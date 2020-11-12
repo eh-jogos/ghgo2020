39f64ae6fbd01f4a85480c09eb7c9e2d16bd0129
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-11-11 21:56:49 -0300

    Improve Utility scripts
    Removed unused drunk steering method.
    Renamed eh_Utility class_name to follow what I'm using in my other
    tools. Added some methods to help with signal connection.

--- scripts/utility/eh_Utility.gd
+++ scripts/utility/eh_Utility.gd
@@ -30,12 +30,43 @@ static func set_node_as_toplevel(node: CanvasItem, to_toplevel: bool) -> void:
 	else:
 		current_pos = node.global_position
 	
-	node.set_as_toplevel(true)
+	node.set_as_toplevel(to_toplevel)
 	
 	if node is Control:
 		node.rect_global_position = current_pos
 	else:
 		node.global_position = current_pos
+
+
+static func connect_signal(
+		from_object: Object, 
+		signal_name: String, 
+		to_object: Object, 
+		func_name: String,
+		binds: = [],
+		flag: = 0
+	) -> void:
+	if not from_object.is_connected(signal_name, to_object, func_name):
+		var error = from_object.connect(signal_name, to_object, func_name, binds, flag)
+		if error != 0:
+			push_error("Signal Connection Failed: %s, %s, %s, %s | Error: %s"%[
+				signal_name,
+				from_object,
+				to_object,
+				func_name,
+				error
+			])
+
+
+static func disconnect_signal(
+	from_object: Object, 
+	signal_name: String, 
+	to_object: Object, 
+	func_name: String
+	) -> void:
+	if from_object.is_connected(signal_name, to_object, func_name):
+		from_object.disconnect(signal_name, to_object, func_name)
+
 
 ### -----------------------------------------------------------------------------------------------
 
