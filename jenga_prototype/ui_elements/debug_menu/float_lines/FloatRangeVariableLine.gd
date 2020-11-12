f03406a50657b38133ac066dc1c779f900c04f53
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-11-11 22:11:18 -0300

    Add Component Scenes and scripts to help build a Debug Menu
    These components usually take advantage of shared variables
    either to just display their values or to modify them and by design,
    the game.

---
+++ jenga_prototype/ui_elements/debug_menu/float_lines/FloatRangeVariableLine.gd
@@ -0,0 +1,54 @@
+# Write your doc string for this file here
+class_name FloatRangeVariableDebugLine
+extends VBoxContainer
+
+### Member Variables and Dependencies -------------------------------------------------------------
+#--- signals --------------------------------------------------------------------------------------
+
+#--- enums ----------------------------------------------------------------------------------------
+
+#--- constants ------------------------------------------------------------------------------------
+
+#--- public variables - order: export > normal var > onready --------------------------------------
+
+export var field_name: String = ""
+export(String, FILE, "*.tres") var float_variable_path: String = ""
+export var min_value: float = 0
+export var max_value: float = 1
+export var step: float = 0.1
+
+#--- private variables - order: export > normal var > onready -------------------------------------
+
+onready var _label: Label = $Label
+onready var _spin_box: SpinBox = $SpinBox
+onready var _float_variable: FloatVariable = load(float_variable_path)
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Built in Engine Methods -----------------------------------------------------------------------
+
+func _ready():
+	_label.text = field_name
+	
+	_spin_box.min_value = min_value
+	_spin_box.max_value = max_value
+	_spin_box.step = step
+	_spin_box.value = _float_variable.value
+	
+	eh_Utility.connect_signal(_spin_box, "value_changed", self, "_on_spin_box_value_changed")
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Public Methods --------------------------------------------------------------------------------
+
+### -----------------------------------------------------------------------------------------------
+
+
+### Private Methods -------------------------------------------------------------------------------
+
+func _on_spin_box_value_changed(p_value: float) -> void:
+	_float_variable.value = p_value
+
+### -----------------------------------------------------------------------------------------------
