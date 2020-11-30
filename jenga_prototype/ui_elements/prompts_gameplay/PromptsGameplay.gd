e889337437df47a05434bbbc87bc9d671b6ac6c7
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-11-30 09:53:38 -0300

    Add cursor images and logic

--- jenga_prototype/ui_elements/prompts_gameplay/PromptsGameplay.gd
+++ jenga_prototype/ui_elements/prompts_gameplay/PromptsGameplay.gd
@@ -19,6 +19,12 @@ onready var tween: Tween = $Tween
 
 #--- private variables - order: export > normal var > onready -------------------------------------
 
+export var _nodepath_mouse_guide: Resource
+export var _nodepath_cursor: Resource
+
+var _mouse_guide: Sprite
+var _cursor: Sprite
+
 ### -----------------------------------------------------------------------------------------------
 
 
@@ -53,3 +59,25 @@ func _set_is_collapsed(value: bool) -> void:
 	tween.start()
 
 ### -----------------------------------------------------------------------------------------------
+
+
+func _on_PromptsGameplay_mouse_entered():
+	if not _cursor:
+		_cursor = get_node(_nodepath_cursor.value)
+	if not _mouse_guide:
+		_mouse_guide = get_node(_nodepath_mouse_guide.value)
+	_cursor.hide()
+	_mouse_guide.hide()
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_PromptsGameplay_mouse_exited():
+	if not _cursor:
+		_cursor = get_node(_nodepath_cursor.value)
+	if not _mouse_guide:
+		_mouse_guide = get_node(_nodepath_mouse_guide.value)
+	_cursor.show()
+	_mouse_guide.show()
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
